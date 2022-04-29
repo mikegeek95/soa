165b700b0bc174eeb347bf83540c9db926f43439
mikael <mikeromero95.geek@gmail.com>
2022-04-29 02:54:50 -0500

    nuevo

---
+++ servicio.php
@@ -0,0 +1,52 @@
+<?php
+require_once("clases/conexcion.php");
+require_once('clases/class.Funciones.php');
+
+
+try
+{
+	$db= new MySQL();
+	$f = new Funciones();	
+	
+	$db->begin();
+	
+	$id = $_GET['id'];
+	
+	$query =("select os.nombre, os.direccion, os.precio, os.horario_apertura, os.horario_cierre, t.nombre as tipo from lugaresocio as os, tipo as t where t.idtipo=os.tipo and os.pueblo=$id");
+	
+	//echo($query);
+	
+	$result=$db->consulta($query);
+	$result_num=$db->num_rows($result);
+	$result_row=$db->fetch_assoc($result);
+	
+	if( $result_num  != 0)
+	{
+		do{
+			
+			$data['LugaresOsico'][] = $result_row;
+			
+
+			
+		}while($result_row=$db->fetch_assoc($result));
+	}
+	
+	else {
+		$data['LugaresOsio'][] = $result_row;
+	}
+	
+	echo (json_encode($data));
+
+
+	$db->commit();
+	
+	
+	
+}
+catch(Exception $e)
+{
+	$db->rollback();
+	
+	echo $e ;
+}
+?>
